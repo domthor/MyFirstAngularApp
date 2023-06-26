f05fc2d5e2a9aa556de4215578b7e20619626fd3
Dominick <domthor@umich.edu>
2023-06-26 17:41:29 -0400

    Added navbar and container components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
