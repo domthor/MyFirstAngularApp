a23a9a19037adf833ccb3705c40afcd33f92f215
Dominick <domthor@umich.edu>
2023-07-06 18:34:53 -0400

    no more custom cursor

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,19 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const cursor = document.createElement('div');
-  const cursorShadow = document.createElement('div');
-  cursorShadow.className = 'cursor-shadow';
-  cursor.className = 'cursor';
-  document.body.appendChild(cursorShadow);
-  document.body.appendChild(cursor);
+// document.addEventListener('DOMContentLoaded', () => {
+//   const cursor = document.createElement('div');
+//   const cursorShadow = document.createElement('div');
+//   cursorShadow.className = 'cursor-shadow';
+//   cursor.className = 'cursor';
+//   document.body.appendChild(cursorShadow);
+//   document.body.appendChild(cursor);
 
-  document.addEventListener('mousemove', (event) => {
-    cursorShadow.style.left = `${event.clientX}px`;
-    cursorShadow.style.top = `${event.clientY}px`;
-    cursor.style.left = `${event.clientX}px`;
-    cursor.style.top = `${event.clientY}px`;
-  });
-});
+//   document.addEventListener('mousemove', (event) => {
+//     cursorShadow.style.left = `${event.clientX}px`;
+//     cursorShadow.style.top = `${event.clientY}px`;
+//     cursor.style.left = `${event.clientX}px`;
+//     cursor.style.top = `${event.clientY}px`;
+//   });
+// });
 
